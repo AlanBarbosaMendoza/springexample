89ce5e701c6f3abe6955e817cb9b33fd71ee686b
AlanBarbosaMendoza <45798473+AlanBarbosaMendoza@users.noreply.github.com>
2022-12-21 14:13:34 -0600

    Mostrar logs en consola
    mostrar logs en consola

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
+        System.out.println("Este es el log de mi microservicio");
         return "Ya casi terminamos!!!! resistan!!!!!!!!!!";
     }
 
