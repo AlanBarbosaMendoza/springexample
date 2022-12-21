8fef1be05d18d302fb6c83acee5b36f062c12b54
AlanBarbosaMendoza <45798473+AlanBarbosaMendoza@users.noreply.github.com>
2022-12-21 13:57:01 -0600

    se realiza una modificación
    modificación para probar

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hola IPN desde Openshift!!!!!!";
     }
 
     public static void main(String[] args) {
