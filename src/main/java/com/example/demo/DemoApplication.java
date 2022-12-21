26d2fff4c1d09b5094f65c3905f653416221fc1d
AlanBarbosaMendoza <45798473+AlanBarbosaMendoza@users.noreply.github.com>
2022-12-21 13:55:50 -0600

    Se modifica el mensaje a mostrar
    Para validar el microservicio y verificar que lo pude modificar, se realiza un cambio.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Alan, Hola mundo desde mi primer microservico en Java 17 desde el IPN!!!!!!!";
     }
 
     public static void main(String[] args) {
